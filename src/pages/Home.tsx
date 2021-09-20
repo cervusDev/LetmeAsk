647d2b435d91f0eb47efe6f56e72080fad573610
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-09-20 14:23:40 -0300

    feat(pages): tela de autenticaçao implementada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,33 @@
-import illustation from '../assets/illustration.svg';
-import logo from '../assets/logo.svg';
-import googleImg from '../assets/google-icon.svg';
+import { Button } from "../components/Button";
+
+import illustation from "../assets/illustration.svg";
+import logo from "../assets/logo.svg";
+import googleImg from "../assets/google-icon.svg";
 
 import "../styles/auth.scss";
 
 export function Home() {
   return (
-    <div id = "page-auth">
+    <div id="page-auth">
       <aside>
-        <img src = {illustation} alt = "illustration"/>
+        <img src={illustation} alt="illustration" />
         <strong>Crie salas de Q&amp;A ao-vivo </strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div className =  "main-content">
-          <img src = {logo} alt = "letmeask"/>
-          <button>
-            <img src = {googleImg} alt = "logo google"/>
+        <div className="main-content">
+          <img src={logo} alt="letmeask" />
+          <button className="create-room">
+            <img src={googleImg} alt="logo google" />
             Crie sua sala
           </button>
-          <div className = "separator"> ou entre em uma sala </div>
+          <div className="separator"> ou entre em uma sala </div>
           <form>
-            <input
-              type = "text"
-              placeholder = "Digite o código da sala"
-            />
-            <button type = "submit"> 
-            Entrar na sala</button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
           </form>
-          </div>
+        </div>
       </main>
     </div>
   );
-};
+}
